bbcfafae3a30b243a2b87f795538fa5fbe1a1164
15188369726@163.com <15188369726@163.com>
2019-07-18 15:22:33 +0800

    'deploy1'

---
+++ precache-manifest.0f24d837505b56d2af3f3bd799f1c5f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a284243652fb16d3db57",
+    "url": "js/chunk-vendors.3187719c.js"
+  },
+  {
+    "revision": "5280bc81ca448acb7032",
+    "url": "js/app.9e0d475a.js"
+  },
+  {
+    "revision": "b6411af25811d36656a7",
+    "url": "js/about.0bb3f1ec.js"
+  },
+  {
+    "revision": "25ff95a2f2e6e7f7fb2fb01d1afe07d6",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5280bc81ca448acb7032",
+    "url": "css/app.784ba48d.css"
+  }
+];
